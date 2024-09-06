b41d13342134213ad2021b45cb1d459e7c9d84eb
Dima-kzk <dimakzk@yahoo.com>
2024-09-06 12:24:37 +0300

    fix port

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = require("./app");
 const DB_HOST =
   "mongodb+srv://Chet:ZhUhEJgiQ8jHfXCl@firstcluster.gcu0u.mongodb.net/books_reader?retryWrites=true&w=majority&appName=FirstCluster";
 
+const port = process.env.PORT || 4000;
+
 mongoos.set("strictQuery", true);
 mongoos
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => console.log("server is running"));
+    app.listen(port, () => console.log("server is running"));
   })
   .catch((error) => {
     console.log(error.message);
